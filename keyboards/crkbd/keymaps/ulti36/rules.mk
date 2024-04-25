7108addb5a6b33371eb05da860185748be4d1628
Dessel Bane <desselbane999+github@gmail.com>
2024-04-26 00:09:53 +0200

    initial keymap crone

---
+++ keyboards/crkbd/keymaps/ulti36/rules.mk
@@ -0,0 +1,29 @@
+# From keyboard dont edit
+# RGBLIGHT_SUPPORTED = yes
+# RGB_MATRIX_SUPPORTED = yes
+
+
+COMMAND_ENABLE 			= no # Commands for debug and configuration
+SPACE_CADET_ENABLE 	= no
+CONSOLE_ENABLE 			= no # Console for debug
+MAGIC_ENABLE 				= no # MAGIC actions (BOOTMAGIC without the boot)
+BOOTMAGIC_ENABLE 		= no # Enable Bootmagic Lite
+MOUSEKEY_ENABLE			= no # Mouse keys
+NKRO_ENABLE					= no # USB N-Key Rollover - if this doesn’t work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+RING_BUFFERED_6KRO_REPORT_ENABLE	= no # USB 6-Key Rollover - Instead of stopping any new input once 6 keys are pressed, the oldest key is released and the new key is pressed.
+AUDIO_ENABLE				= no # Enable the audio subsystem.
+KEY_OVERRIDE_ENABLE	= no # Enable the key override feature
+MIDI_ENABLE					= no # MIDI controls
+UNICODE_ENABLE			= no # Unicode
+VIA_ENABLE      		= no # Enable VIA
+
+
+COMBO_ENABLE			= yes # Key combo feature
+EXTRAKEY_ENABLE		= yes # Audio control and System control
+LTO_ENABLE      	= yes
+LEADER_ENABLE 		= yes # Enable leader key chording
+TAP_DANCE_ENABLE 	= yes
+
+# RGB
+RGBLIGHT_ENABLE = yes
+RGB_MATRIX_ENABLE = no
