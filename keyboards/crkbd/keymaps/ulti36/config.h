9a5ee3e435c7848aee895fafaffbf0bdf9653d65
Dessel Bane <desselbane999+github@gmail.com>
2024-04-28 01:10:05 +0200

    feat add mouse and media buttons/layer

--- keyboards/crkbd/keymaps/ulti36/config.h
+++ keyboards/crkbd/keymaps/ulti36/config.h
@@ -40,6 +40,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef TAPPING_TERM
 #define TAPPING_TERM 170
 
+// Mouse
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
+
+#define MK_C_OFFSET_0	1	        //Cursor offset per movement (KC_ACL0)
+#define MK_C_INTERVAL_0	32	    //Time between cursor movements (KC_ACL0)
+
+#define MK_C_OFFSET_1	4	        //Cursor offset per movement (KC_ACL1)
+#define MK_C_INTERVAL_1	16	    //Time between cursor movements (KC_ACL1)
+
+#define MK_C_OFFSET_UNMOD	8 	  //Cursor offset per movement (unmodified)
+#define MK_C_INTERVAL_UNMOD	16	//Time between cursor movements (unmodified)
+
+#define MK_C_OFFSET_2	32	      //Cursor offset per movement (KC_ACL2)
+#define MK_C_INTERVAL_2	16	    //Time between cursor movements (KC_ACL2)
+
+#define MK_W_OFFSET_0	1         //Scroll steps per scroll action (KC_ACL0)
+#define MK_W_INTERVAL_0	200	    //Time between scroll steps (KC_ACL0)
+
+#define MK_W_OFFSET_1	1	        //Scroll steps per scroll action (KC_ACL1)
+#define MK_W_INTERVAL_1	60	    //Time between scroll steps (KC_ACL1)
+
+#define MK_W_OFFSET_UNMOD	1	    //Scroll steps per scroll action (unmodified)
+#define MK_W_INTERVAL_UNMOD	20	//Time between scroll steps (unmodified)
+
+#define MK_W_OFFSET_2	2	        //Scroll steps per scroll action (KC_ACL2)
+#define MK_W_INTERVAL_2	10	    //Time between scroll steps (KC_ACL2)
+
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_LIMIT_VAL 120
 #    define RGBLIGHT_HUE_STEP 10
