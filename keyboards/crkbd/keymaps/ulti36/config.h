fddbd4cc352ae556a21eb943547edacfe6eb2bc6
Dessel Bane <desselbane999+github@gmail.com>
2024-06-21 13:24:29 +0200

    remove RGBLIGHT_SPLIT

--- keyboards/crkbd/keymaps/ulti36/config.h
+++ keyboards/crkbd/keymaps/ulti36/config.h
@@ -74,5 +74,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_SAT_STEP 17
 #    define RGBLIGHT_VAL_STEP 17
 #    define RGBLIGHT_SLEEP
-#    define RGBLIGHT_SPLIT
 #endif
