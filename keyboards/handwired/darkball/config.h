635a00a52db2926a01d30a5b0db268e677c5b2d9
Dessel Bane <desselbane999+github@gmail.com>
2025-01-26 17:00:49 +0100

    first attempt handwired

---
+++ keyboards/handwired/darkball/config.h
@@ -0,0 +1,30 @@
+/*
+Copyright 2020 Richard Sutherland
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define SPI_PORT spi0
+
+// #define PIN_MISO 4
+// #define PIN_CS   9
+// #define PIN_SCK  2
+// #define PIN_MOSI 3
+
+#define PMW33XX_CS_PIN GP9
+#define POINTING_DEVICE_MOTION_PIN GP1
+
+#define POINTING_DEVICE_INVERT_X
---
+++ keyboards/handwired/darkball/keymaps/default/keymap.c
@@ -0,0 +1,36 @@
+/* Copyright 2020 Richard Sutherland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "print.h"
+
+// Dummy
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {{{ KC_NO }}};
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  debug_mouse=true;
+
+  //pointing_device_set_cpi(12000);
+
+  print("post initializing\n");
+}
+
+void housekeeping_task_user(void) {
+  //dprint("housekeeping\n");
+}
