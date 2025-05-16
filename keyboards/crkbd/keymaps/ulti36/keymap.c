9bada32cac2699b2ecabb7dec89dbe409c0a4dc5
Dessel Bane <desselbane999+github@gmail.com>
2025-05-16 09:02:48 +0200

    fine tune chordal hold to allow ctr+a alt+tab and alt+space

--- keyboards/crkbd/keymaps/ulti36/keymap.c
+++ keyboards/crkbd/keymaps/ulti36/keymap.c
@@ -324,3 +324,33 @@ void leader_end_user(void) {
 
    layer_state_set_user(_BASE);
 }
+
+
+// Fine tune Chordal Hold
+// https://docs.qmk.fm/tap_hold#per-chord-customization
+
+bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
+                      uint16_t other_keycode, keyrecord_t* other_record) {
+    // Exceptionally allow some one-handed chords for hotkeys.
+    switch (tap_hold_keycode) {
+        case MT(MOD_LALT, KC_R):
+            if (other_keycode == KC_SPACE) {
+                return true;
+            }
+            break;
+
+        case MT(MOD_LALT, KC_I):
+            if (other_keycode == LT(_NUM, KC_TAB)) {
+                return true;
+            }
+            break;
+
+        case MT(MOD_LCTL, KC_T):
+            if (other_keycode == MT(MOD_LGUI,KC_A)) {
+                return true;
+            }
+            break;
+    }
+    // Otherwise defer to the opposite hands rule.
+    return get_chordal_hold_default(tap_hold_record, other_record);
+}
