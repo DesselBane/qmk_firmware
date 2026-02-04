4b805cde19514290d1d8c3bbc0be4b04a7265a77
Dessel Bane <desselbane999+github@gmail.com>
2026-02-04 23:29:05 +0100

    fix after hard reset sync
    Signed-off-by: Dessel Bane <desselbane999+github@gmail.com>

---
+++ keyboards/crkbd/keymaps/ulti36/config.h
@@ -0,0 +1,99 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+// enum LAYERS {
+//   _BASE,
+//   _MOUSE,
+//   _NAV, 2
+//   _NUM, 3
+//   _SYM, 4
+//   _FUN, 5
+//   _GAME,
+//   _GAME2,
+//   _GAME_FN,
+// };
+
+// Tri Layer
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 4
+#define TRI_LAYER_ADJUST_LAYER 5
+
+// #define LEADER_PER_KEY_TIMING
+// #define LEADER_TIMEOUT 200
+// #define LEADER_NO_TIMEOUT
+
+// #define QUICK_TAP_TERM 0
+// #define TAPPING_TERM 100
+#undef TAPPING_TERM
+#define TAPPING_TERM 170
+
+#define CHORDAL_HOLD // Turn on chordal hold => https://docs.qmk.fm/tap_hold#chordal-hold
+// #define HOLD_ON_OTHER_KEY_PRESS
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
+
+// Mouse
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
+
+#define MK_C_OFFSET_0 1    // Cursor offset per movement (KC_ACL0)
+#define MK_C_INTERVAL_0 32 // Time between cursor movements (KC_ACL0)
+
+#define MK_C_OFFSET_1 4    // Cursor offset per movement (KC_ACL1)
+#define MK_C_INTERVAL_1 16 // Time between cursor movements (KC_ACL1)
+
+#define MK_C_OFFSET_UNMOD 8    // Cursor offset per movement (unmodified)
+#define MK_C_INTERVAL_UNMOD 16 // Time between cursor movements (unmodified)
+
+#define MK_C_OFFSET_2 32   // Cursor offset per movement (KC_ACL2)
+#define MK_C_INTERVAL_2 16 // Time between cursor movements (KC_ACL2)
+
+#define MK_W_OFFSET_0 1     // Scroll steps per scroll action (KC_ACL0)
+#define MK_W_INTERVAL_0 200 // Time between scroll steps (KC_ACL0)
+
+#define MK_W_OFFSET_1 1    // Scroll steps per scroll action (KC_ACL1)
+#define MK_W_INTERVAL_1 60 // Time between scroll steps (KC_ACL1)
+
+#define MK_W_OFFSET_UNMOD 1    // Scroll steps per scroll action (unmodified)
+#define MK_W_INTERVAL_UNMOD 20 // Time between scroll steps (unmodified)
+
+#define MK_W_OFFSET_2 2    // Scroll steps per scroll action (KC_ACL2)
+#define MK_W_INTERVAL_2 10 // Time between scroll steps (KC_ACL2)
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP 10
+#    define RGBLIGHT_SAT_STEP 17
+#    define RGBLIGHT_VAL_STEP 17
+#    define RGBLIGHT_SLEEP
+#endif
